902b6b3b674f46939b9c928317e94f6dec5b1d38
swordnikolav <>
2026-01-13 21:50:14 +0100

    mis dotfiles

--- dotfiles/.config/cmus/lib.pl
+++
@@ -1,2 +0,0 @@
-/home/sword/Música/Dj Hinx - El Viento-NUDXKEwwDas.ogg
-/home/sword/Música/STREET LIFE N METHOD MAN FT - HAVOC FROM MOBB DEEP 'SQUAD UP' POWERED BY - STREETLIFEWU.COM-YQS2jo_8YRU.ogg
